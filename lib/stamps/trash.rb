46133cd9c5700c263ee41066ec01a04ff604a173
hoanghiep90 <hoanghiep.stt@gmail.com>
2015-09-18 09:30:39 +0700

    Ignore DeleverDate

--- lib/stamps/trash.rb
+++ lib/stamps/trash.rb
@@ -15,11 +15,10 @@ module Hashie
     # Sort the hash by the order in which the properties are declared
     def to_hash
       sorted_keys = self.class.properties.map{|prop| prop.to_s} & self.keys
-
       # Use active_support for ordering hashes for Ruby < 1.9
       out = RUBY_VERSION >= '1.9' ? {} : ActiveSupport::OrderedHash.new
       sorted_keys.each do |k|
-        next if self[k].nil?
+        next if self[k].nil? || k == 'DeliveryDate'
         out[k] = Hashie::Hash === self[k] ? self[k].to_hash : self[k]
       end
       out
