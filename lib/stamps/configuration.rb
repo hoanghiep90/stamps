670edd2f81c093d59e86ef907f1f4841a8b47722
nusken <hiepnh@nustechnology.com>
2016-10-28 23:08:29 +0700

    Upgrade Endpoint

--- lib/stamps/configuration.rb
+++ lib/stamps/configuration.rb
@@ -18,10 +18,10 @@ module Stamps
       :endpoint].freeze
 
     # The endpoint that will be used to connect if none is set
-    DEFAULT_ENDPOINT = 'https://swsim.stamps.com/swsim/swsimv29.asmx'.freeze
+    DEFAULT_ENDPOINT = 'https://swsim.stamps.com/swsim/swsimv45.asmx'.freeze
 
     # The default namespace used on Stamps.com wsdl
-    DEFAULT_NAMESPACE = 'http://stamps.com/xml/namespace/2010/11/swsim/swsimv29'
+    DEFAULT_NAMESPACE = 'http://stamps.com/xml/namespace/2015/05/swsim/swsimv45'
 
     # @note JSON is preferred over XML because it is more concise and faster to parse.
     DEFAULT_FORMAT = :hash
